5077dbb154124de227df136f514b4dc486a44b10
Hari Prayudha <prayudahari@gmail.com>
2026-09-23 08:32:13 +0700

    feat: add GreetingSeeder

--- database/seeders/GreetingSeeder.php
+++ database/seeders/GreetingSeeder.php
@@ -16,7 +16,7 @@ class GreetingSeeder extends Seeder
             ['id' => 1],
             [
                 'photo_path' => null,
-                'name' => 'Dr. Dra. Ida Nurhayati, M.Kes.',
+                'name' => ' Dr. Jhonson P. Sihombing, S.Si., M.Sc., Apt.',
                 'position' => 'Kepala Pusat Penjaminan Mutu Poltekkes Kemenkes Medan',
                 'content' => '<p>Selamat datang di portal resmi Pusat Penjaminan Mutu (PPM) Poltekkes Kemenkes Medan. Pusat Penjaminan Mutu memegang mandat strategis dalam mengawal mutu akademik dan tata kelola institusi secara berkelanjutan (Continuous Quality Improvement) demi melahirkan tenaga kesehatan yang profesional, unggul, dan berintegritas.</p><p>Melalui implementasi siklus PPEPP (Penetapan, Pelaksanaan, Evaluasi, Pengendalian, dan Peningkatan), kami memastikan seluruh program studi di lingkungan Poltekkes Kemenkes Medan senantiasa memenuhi bahkan melampaui Standar Nasional Pendidikan Tinggi (SN-Dikti) dan kriteria akreditasi LAM-PTKes menuju predikat Unggul.</p><p>Portal ini kami sediakan sebagai pusat keterbukaan informasi dan repositori resmi Sistem Penjaminan Mutu Internal (SPMI), dokumen SOP, instrumen Audit Mutu Internal (AMI), serta laporan penjaminan mutu bagi seluruh sivitas akademika dan masyarakat.</p>',
             ]
